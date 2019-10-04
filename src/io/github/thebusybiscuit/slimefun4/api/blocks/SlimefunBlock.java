c205b882f737d69ecf9b2abf8c20b76cf57c5006
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-10-04 22:36:50 +0200

    Created JsonDataHolder class

--- src/io/github/thebusybiscuit/slimefun4/api/blocks/SlimefunBlock.java
+++ src/io/github/thebusybiscuit/slimefun4/api/blocks/SlimefunBlock.java
@@ -1,25 +1,18 @@
 package io.github.thebusybiscuit.slimefun4.api.blocks;
 
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.IntFunction;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 
 import io.github.thebusybiscuit.cscorelib2.collections.OptionalMap;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.inventory.DirtyChestMenu;
 
-public class SlimefunBlock {
+public class SlimefunBlock extends JsonDataHolder {
 	
 	private BlockLocation location;
 	private String id;
-	private JsonObject data;
-	private boolean dirty = false;
 	
 	private OptionalMap<String, Object> cache;
 	private Optional<DirtyChestMenu> inventory = Optional.empty();
@@ -29,9 +22,10 @@ public class SlimefunBlock {
 	}
 	
 	public SlimefunBlock(BlockLocation l, String id, JsonObject data) {
+		super(data);
+		
 		this.location = l;
 		this.id = id;
-		this.data = data;
 	}
 	
 	public void setID(String id) {
@@ -57,14 +51,6 @@ public class SlimefunBlock {
 		this.location = newLocation;
 		markDirty();
 	}
-
-	public void markDirty() {
-		dirty = true;
-	}
-	
-	public boolean isDirty() {
-		return dirty;
-	}
 	
 	public String getID() {
 		return id;
@@ -83,125 +69,10 @@ public class SlimefunBlock {
 		data.addProperty("id", id);
 		return data.toString();
 	}
-
-	// Setters
-	public void setString(String key, String value) {
-		data.addProperty(key, value);
-	}
-	
-	public void setInt(String key, int value) {
-		data.addProperty(key, value);
-	}
-	
-	public void setBoolean(String key, boolean value) {
-		data.addProperty(key, value);
-	}
-	
-	public void setFloat(String key, float value) {
-		data.addProperty(key, value);
-	}
-	
-	public void setStringArray(String key, String[] array) {
-		JsonArray json = new JsonArray();
-		for (String value: array) json.add(value);
-		data.add(key, json);
-	}
-	
-	public void setIntArray(String key, int[] array) {
-		JsonArray json = new JsonArray();
-		for (int value: array) json.add(value);
-		data.add(key, json);
-	}
-	
-	public void setBooleanArray(String key, boolean[] array) {
-		JsonArray json = new JsonArray();
-		for (boolean value: array) json.add(value);
-		data.add(key, json);
-	}
-	
-	public void setFloatArray(String key, float[] array) {
-		JsonArray json = new JsonArray();
-		for (float value: array) json.add(value);
-		data.add(key, json);
-	}
-
-	// Getters
-	public Optional<String> getString(String key) {
-		return getPrimitive(key, JsonElement::getAsString);
-	}
-	public String getString(String key, String defaultValue) {
-		return getString(key).orElse(defaultValue);
-	}
-	
-	public Optional<Integer> getInt(String key) {
-		return getPrimitive(key, JsonElement::getAsInt);
-	}
-	public int getInt(String key, int defaultValue) {
-		return getInt(key).orElse(defaultValue);
-	}
-	
-	public Optional<Boolean> getBoolean(String key) {
-		return getPrimitive(key, JsonElement::getAsBoolean);
-	}
-	public boolean getBoolean(String key, boolean defaultValue) {
-		return getBoolean(key).orElse(defaultValue);
-	}
-	
-	public Optional<Float> getFloat(String key) {
-		return getPrimitive(key, JsonElement::getAsFloat);
-	}
-	public float getFloat(String key, float defaultValue) {
-		return getFloat(key).orElse(defaultValue);
-	}
-	
-	public Optional<String[]> getStringArray(String key) {
-		return getArray(key, String[]::new, JsonElement::getAsString);
-	}
-	
-	public Optional<Integer[]> getIntArray(String key) {
-		return getArray(key, Integer[]::new, JsonElement::getAsInt);
-	}
-	
-	public Optional<Boolean[]> getBooleanArray(String key) {
-		return getArray(key, Boolean[]::new, JsonElement::getAsBoolean);
-	}
-	
-	public Optional<Float[]> getFloatArray(String key) {
-		return getArray(key, Float[]::new, JsonElement::getAsFloat);
-	}
 	
 	public <T> Optional<T> getCachedObject(String key, Class<T> c) {
 		if (cache == null) return Optional.empty();
 		return cache.get(key).map(c::cast);
-	}
-	
-	protected <T> Optional<T[]> getArray(String key, IntFunction<T[]> constructor, Function<JsonElement, T> getter) {
-		JsonElement element = data.get(key);
-		
-		if (element == null || !(element instanceof JsonArray)) {
-			return Optional.empty();
-		}
-		else {
-			JsonArray json = (JsonArray) element;
-			T[] array = constructor.apply(json.size());
-			
-			for (int i = 0; i < array.length; i++) {
-				array[i] = getter.apply(json.get(i));
-			}
-			
-			return Optional.of(array);
-		}
-	}
-	
-	protected <T> Optional<T> getPrimitive(String key, Function<JsonElement, T> getter) {
-		JsonElement element = data.get(key);
-		
-		if (element == null || !(element instanceof JsonPrimitive)) {
-			return Optional.empty();
-		}
-		else {
-			return Optional.of(getter.apply(element));
-		}
 	}
 
 }
